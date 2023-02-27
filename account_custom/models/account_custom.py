9e74103148d82531883e3660a02af97a49781b7f
ahmedoosama <ahmed.os.moh@gmail.com>
2023-02-27 12:56:54 +0300

    [MOD] account

--- account_custom/models/account_custom.py
+++ account_custom/models/account_custom.py
@@ -12,7 +12,7 @@ class AccountMove(models.Model):
     project_amount = fields.Float(string='Rental Price',store=True)
     name = fields.Char(string='Label', tracking=True, default="/")
 
-    # @api.onchange('co_per', 'project_amount')
-    # def _onchange_field_name(self):
-    #     if self.co_per and self.project_amount :
-    #         self.price_unit = self.co_per * self.project_amount / 100
+    @api.onchange('co_per', 'project_amount')
+    def _onchange_field_name(self):
+        if self.co_per and self.project_amount :
+            self.price_unit = self.co_per * self.project_amount / 100
